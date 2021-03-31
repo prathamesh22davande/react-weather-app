c271df324848f50e5296f5609cab4ba7cf672003
prathamesh22davande <prathamesh22davande@gmail.com>
2021-03-31 23:02:01 +0530

    Added css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import cloudy from "./images/cloudy.jpg";
 
 function App() {
+  const [text, setText] = useState("");
+  const [city, setCity] = useState("ichalkaranji");
+  const [data, setData] = useState(0);
+  const [location, setLocation] = useState({
+    lat: 16.6886,
+    lon: 74.4593,
+  });
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://api.openweathermap.org/data/2.5/weather?lat=" +
+  //         location.lat +
+  //         "&lon=" +
+  //         location.lon +
+  //         "&appid=923aed81035e9578514d169ddfd34e5c"
+  //     )
+  //     .then((fetchedData) => {
+  //       console.log(fetchedData);
+  //       setData(fetchedData);
+  //     })
+  //     .catch((e) => {
+  //       console.log("Error occured" + e);
+  //     });
+  // }, [location]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&appid=923aed81035e9578514d169ddfd34e5c"
+      )
+      .then((fetchedData) => {
+        console.log(fetchedData);
+        setData(fetchedData);
+      })
+      .catch((e) => {
+        console.log("error occured" + e);
+      });
+  }, [city]);
+
+  if (data) {
+    console.log(parseFloat(data.data.main.temp) - 273.15);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Weather</h1>
+        <div>
+          <input
+            className="input-city"
+            type="text"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              console.log(text);
+              setCity(text);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div>
+          {data ? (
+            <>
+              <h1 className="weather-data">
+                {(parseFloat(data.data.main.temp) - 273.15).toFixed(2)}°
+              </h1>
+              <h2>{data.data.name}</h2>
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </main>
     </div>
   );
 }
